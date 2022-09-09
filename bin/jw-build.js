57588c89f9e9862701ae9e0a7f40808e24c1b967
叶继伟 <729721503@qq.com>
2022-09-10 03:51:21 +0800

    node版本校验 添加commander semver库

--- bin/jw-build.js
+++ bin/jw-build.js
@@ -1,3 +1,16 @@
 #!/usr/bin/env node
+const checkNode = require("../lib/checkNode");
+const pkg = require("../package.json");
 
-console.log("jw-build");
+const MIN_NODE_VERSION = "8.9.0";
+(async () => {
+  try {
+    if (!checkNode(">=" + MIN_NODE_VERSION)) {
+      throw (
+        new Error("Please upgrade your node version to v") + MIN_NODE_VERSION
+      );
+    }
+  } catch (e) {
+    console.log(e);
+  }
+})();
