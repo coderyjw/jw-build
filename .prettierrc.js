1be1fe244fba446ecc628ae357695a0dc4826512
叶继伟 <yejw6@asiainfo.com>
2022-09-22 17:35:13 +0800

    webpack loader配置移植

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,8 +1,8 @@
 module.exports = {
-  useTabs: false,
-  tabWidth: 2,
-  printWidth: 120,
-  singleQuote: false,
-  trailingComma: "none",
-  semi: true
+  // useTabs: false,
+  // tabWidth: 2,
+  // printWidth: 120,
+  // singleQuote: false,
+  // trailingComma: "none",
+  // semi: true
 };
--- plugins/initPlugins.js
+++ plugins/initPlugins.js
@@ -1,4 +1,6 @@
 const path = require("path");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
 module.exports = function (api, options) {
   const { getWebpackConfig, log } = api;
   const config = getWebpackConfig();
@@ -7,11 +9,52 @@ module.exports = function (api, options) {
   const mode = process.env.JW_BUILD_MODE || "development";
   config.mode(mode);
 
-  // 设置entry
+  /* 设置entry */
   config.entry("index").add(path.resolve(dir, "./src/index.js")).end();
 
-  // 设置output
+  /* 设置output */
   config.output.filename("js/[name].js").path(path.resolve(dir, "./dist"));
+
+  /* 设置module */
+  // 1. css-loader mini-css
+  config.module
+    .rule("css")
+    .test(/\.css$/)
+    .exclude.add("/node_modules/")
+    .end()
+    .use("mini-css")
+    .loader(MiniCssExtractPlugin.loader)
+    .end()
+    .use("css-loader")
+    .loader("css-loader")
+    .end();
+
+  // 2. 图片
+  config.module
+    .rule("asset")
+    .test(/\.(png|jpe?g|svg|gif)$/i)
+    .type("asset")
+    .parser({
+      dataUrlCondition: {
+        maxSize: 8 * 1024,
+      },
+    });
+
+  config.module.rule("asset").set("generator", {
+    filename: "images/[name].[hash:6][ext]",
+  });
+
+  // 3. ejs
+  config.module
+    .rule("ejs")
+    .test(/\.ejs$/)
+    .exclude.add("/node_modules/")
+    .end()
+    .use("ejs-loader")
+    .loader("ejs-loader")
+    .options({
+      esModule: false,
+    });
 
   log.verbose("内置webpack配置", config.toConfig());
 };
